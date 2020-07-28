411b00bd61adc91d9a871c549e1bea3a683c5c35
김승규 <rlatmdrb1996@koreatech.ac.kr>
2020-07-28 18:14:58 +0900

    val -> let

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,10 +1,10 @@
-var night = document.getElementById('night');
+let night = document.getElementById('night');
 night.addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = 'black';
     document.querySelector('body').style.color = 'white';
 })
 
-var day = document.getElementById('day');
+let day = document.getElementById('day');
 day.addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = 'white';
     document.querySelector('body').style.color = 'black';
